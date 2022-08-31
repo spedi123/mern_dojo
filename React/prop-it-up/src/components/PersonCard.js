78bb0e8155771a8419128c7ced74f6cf2463b443
spedi123 <peteran8604@gmail.com>
2022-08-31 16:49:48 -0700

    putting it together assignment

--- React/prop-it-up/src/components/PersonCard.js
+++ React/prop-it-up/src/components/PersonCard.js
@@ -2,17 +2,32 @@ import React, { Component } from 'react';
     
     
 export class PersonCard extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            plusAge : this.props.age
+        }
+    }
+
+    clickBirth = () => {
+        this.setState({
+            plusAge : this.state.plusAge +1     
+        })
+    }
     render() {
+        const {lastName, firstName, hairColor} = this.props
+        const {plusAge} = this.state
         return (
             <div>
                 <h1>
-                    {this.props.lastName}, {this.props.firstName}
+                    {lastName}, {firstName}
                 </h1>
-                <p>Age: {this.props.age}</p>
-                <p>Hair Color: {this.props.hairColor}</p>
+                <p>Age: {plusAge}</p>
+                <p>Hair Color: {hairColor}</p>
+                <button onClick={this.clickBirth}>Birthday button for {firstName} {lastName}</button>
             </div>
         )
-    }
+    }   
 }
     
 export default PersonCard;
