a2641c33d48b33a48882ebff34c8754dcacf09b8
spedi123 <peteran8604@gmail.com>
2022-08-31 13:36:38 -0700

    update time to duel

--- javascript/timeToDuel.js
+++ javascript/timeToDuel.js
@@ -1,12 +1,12 @@
 class Card {
-    constructor(name, cost) {
+    constructor(name="", cost = 0) {
         this.name = name;
         this.cost = cost;
     }
 }
 
 class Unit extends Card {
-    constructor(name, cost, res, power) {
+    constructor(name="", cost = 0, res= 0, power= 0) {
         super(name, cost);
         this.res = res;
         this.power = power;
@@ -18,17 +18,29 @@ class Unit extends Card {
 }
 
 class Effect extends Card {
-    constructor(name, cost, text, stat, magnitude) {
+    constructor(name="", cost= 0, text="", stat="", magnitude= 0) {
         super(name, cost);
         this.text = text;
         this.stat = stat;
         this.magnitude = magnitude;
     }
+
+    playEffect(target){
+        if (this.stat === "res") {
+            target.res += this.magnitude;
+        } else if (this.stat === "power") {
+            target.power += this.magnitude;
+        }
+    }
 }
 
-const unit1 = new Unit ('Red Belt Ninja', 3, 3, 4)
-const unit2 = new Unit ('Black Belt Ninja', 4, 5, 4)
+const unit1 = new Unit ('Red Belt Ninja', 3, 4, 3)
+const unit2 = new Unit ('Black Belt Ninja', 4, 4, 5)
 
 const effect1 = new Effect('Hard Algorithm', 2, 'increase target\'s resilience by 3', 'resilience', +3)
 const effect2 = new Effect('Unhandled Promise Rejection', 1, 'reduce target\'s resilience by 2', 'resilience', -2)
 const effect3 = new Effect('Pair Programming', 3, 'increase target\'s power by 2', 'power', +2)
+
+unit1.attack(unit2)
+console.log(unit1)
+console.log(unit2)
