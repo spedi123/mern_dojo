31e50b2148ab123984d70fc183ecbaf886aa8c80
spedi123 <peteran8604@gmail.com>
2022-09-08 13:31:17 -0700

    routing_practice assignment

--- React/routing_practice/src/App.js
+++ React/routing_practice/src/App.js
@@ -1,41 +1,37 @@
-import { Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { useParams } from 'react-router-dom';
 import React from 'react';
-import './App.css';
 
 function App() {
+    const NumberColor = () => {
+        const { val } = useParams();
 
-  const valFromUrl = () => {
-    const {val} = useParams;
+        if( isNaN(+val) ) { return <Word /> }
+        else { return <Number /> }
+    }
 
-    if( isNaN(+val) ) { return <Word /> }
-    else { return <Num /> }
-  }
+    const Number = () => {
+        const { val } = useParams();
+        return <h1>The Number is: { val }</h1>
+    }
+    const Word = () => {
+        const { val } = useParams();
+        return <h1>The word is: { val }</h1>
+    }
 
-  const Num = () => {
-    const {val} = useParams;
-      return <h2>The number is : {val}</h2>
-  }
+    const Color = () => {
+        const { word, text, bckgnd } = useParams();
+        return <h1 style={{ color: text, backgroundColor: bckgnd }}>The word is: { word }</h1>
+    }
 
-  const Word = () => {
-    const {val} = useParams;
-      return <h2>The word is : {val}</h2>
-  }
-
-  const wordColor = () => {
-    const {word, wordColor, bgColor} = useParams;
-    return <h2 style={{color:wordColor, backgroundColor:bgColor}}>The word is : {word}</h2>
-  }
-
-  return (
-    <div>
-      <Routes>
-        <Route path="/home" element={<h2>Welcome!!!</h2>}/>
-        <Route path="/:val" element={valFromUrl}/>
-        <Route path="/:word/:wordColor/:bgColor" element={wordColor}/>
-      </Routes>
-    </div>
-  );
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/home" element={ <h1>Welcome!</h1> } />
+                <Route path="/:val" element={ <NumberColor /> } />
+                <Route path="/:word/:text/:bckgnd" element={ <Color /> } />
+            </Routes>
+        </BrowserRouter>
+    );
 }
-
 export default App;
