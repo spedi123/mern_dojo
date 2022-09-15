14dcaaf56570763582242d38f30ece9ba19ffa54
spedi123 <peteran8604@gmail.com>
2022-09-15 01:10:41 -0700

    service setup

--- Full_Stack_MERN/team_manager/server/services/player.service.js
+++ Full_Stack_MERN/team_manager/server/services/player.service.js
@@ -1,72 +1,43 @@
-const { Destination } = require('../models/destination.model');
+const { Player } = require('../models/player.model');
 
-const createDestination = async (data) => {
-  console.log('service: createDestination');
+const createPlayer = async (data) => {
+  console.log('service: createPlayer');
 
-  // Use the mongoose model to interact with the database.
-  const destination = await Destination.create(data);
-  return destination;
+  const player = await Player.create(data);
+  return player;
 };
 
-const getAllDestinations = async () => {
-  const destinations = await Destination.find();
-  return destinations;
+const getAllPlayers = async () => {
+  const players = await Player.find();
+  return players;
 };
 
-const getDestinationById = async (id) => {
-  const destination = await Destination.findById(id);
-  return destination;
+const getPlayerById = async (id) => {
+  const player = await Player.findById(id);
+  return player;
 };
 
-const deleteDestinationById = async (id) => {
-  const destination = await Destination.findByIdAndDelete(id);
-  return destination;
+const deletePlayerById = async (id) => {
+  const player = await Player.findByIdAndDelete(id);
+  return player;
 };
 
-const updateDestinationById = async (id, data) => {
-  const destination = await Destination.findByIdAndUpdate(id, data, {
+const updatePlayerById = async (id, data) => {
+  const player = await Player.findByIdAndUpdate(id, data, {
     // Re-run validations.
     runValidators: true,
-    // Return the updated destination.
+    // Return the updated Player.
     new: true,
   });
 
-  return destination;
+  return player;
 };
 
-const deleteDestinationById2 = async (id) => {
-  // findByIdAndDelete performs both tasks together for us, without it, it
-  // would look like this.
 
-  // Or reuse our findDestinationById function.
-  const foundDestination = await Destination.findById(id);
-
-  if (foundDestination === null) {
-    return null;
-  }
-
-  const deletedDestination = await foundDestination.remove();
-  return deletedDestination;
-};
-
-// Not needed on exam, used to load lot's of data so we can travel.
-const createManyDestinations = async (documents) => {
-  // Don't await inside a loop, it will delay iteration.
-  const createPromises = documents.map((document) =>
-    createDestination(document)
-  );
-  // The one resulting promise will be awaited by the caller of this function.
-  return Promise.allSettled(createPromises);
-};
-
-// Export all the service functions in an object.
 module.exports = {
-  // long-form - key: value
-  createDestination: createDestination,
-  // Shorthand can be used when key name matches var name
-  getAllDestinations,
-  getDestinationById,
-  deleteDestinationById,
-  updateDestinationById,
-  createManyDestinations,
+  createPlayer,
+  getAllPlayers,
+  getPlayerById,
+  deletePlayerById,
+  updatePlayerById,
 };
