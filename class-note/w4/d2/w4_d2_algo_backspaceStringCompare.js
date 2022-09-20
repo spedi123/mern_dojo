f2bbf58a512114f3042e76a793e2f1a99ee7ef10
spedi123 <peteran8604@gmail.com>
2022-09-20 08:50:02 -0700

    w4_d2 algo

---
+++ class-note/w4/d2/w4_d2_algo_backspaceStringCompare.js
@@ -0,0 +1,64 @@
+/* 
+  Given two strings S and T containing only lowercase letters and "#" characters,
+  return if they are equal when both are typed into empty text editors.
+  # character means a backspace character.
+
+  i.e., after processing the backspaces, are the two strings equal?
+
+  Bonus: solve in O(n) time
+*/
+
+const S1 = 'ab#c';
+const T1 = 'ad#c';
+const expected1 = true;
+// Explanation: Both S and T become "ac"
+
+const S2 = 'ab##';
+const T2 = 'c#d#';
+const expected2 = true;
+// Explanation: Both S and T become ""
+
+const S3 = 'a##c';
+const T3 = '#a#c';
+const expected3 = true;
+// Explanation: Both S and T become "c"
+
+const S4 = 'a#c';
+const T4 = 'b';
+const expected4 = false;
+// Explanation: S becomes "c" while T becomes "b".
+
+/**
+ * Determines if the given strings are equal after the backspace characters
+ * "#" are processed.
+ * - Time: O(?).
+ * - Space: O(?).
+ * @param {string} S
+ * @param {string} T
+ * @returns {boolean} Whether the given strings are equal after backspaces
+ *    have been processed.
+ */
+ function backspaceStringCompare(S, T) {
+	let countS = 0;
+	let countT = 0;
+
+	for (let i = 0; i < S.length; i++) {
+		if (S[i] === '#') {
+			countS++;
+		}
+	}
+	for (let i = 0; i < T.length; i++) {
+		if (T[i] === '#') {
+			countT++;
+		}
+	}
+	if (S.length - countS === T.length - countT) {
+		return true;
+	}
+	return false;
+}
+
+console.log(backspaceStringCompare(S1, T1));
+console.log(backspaceStringCompare(S2, T2));
+console.log(backspaceStringCompare(S3, T3));
+console.log(backspaceStringCompare(S4, T4));
